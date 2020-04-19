147f12035460c907bf39652b5fdc4f67be443133
Ferney Acosta <ferneyacosta@gmail.com>
2020-04-18 23:32:59 -0500

    Modificaciones en los Modulos

--- src/app/pages/graficas1/graficas1.component.ts
+++ src/app/pages/graficas1/graficas1.component.ts
@@ -3,14 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-graficas1',
   templateUrl: './graficas1.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class Graficas1Component implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
